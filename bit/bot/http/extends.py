7fc83c546963eff50e25fe2e18a5b29d72aac8a2
Ryan Northey <ryan@3ca.org.uk>
2012-01-29 12:14:14 +0000

    making plugins extensible

---
+++ bit/bot/http/extends.py
@@ -0,0 +1,25 @@
+
+from zope.interface import implements
+from zope.component import getUtility, provideAdapter
+from bit.core.interfaces import IConfiguration, IPlugin, ISockets, IPluginExtender
+
+class HTTPPlugin(object):
+   implements(IPluginExtender)
+   def __init__(self,plugin):
+      self.plugin = plugin
+
+   def extend(self):
+       if hasattr(self.plugin,'load_HTTP'):
+           getattr(self.plugin,'load_HTTP')()      
+       else:
+           fpath =  os.path.dirname(inspect.getfile(self.plugin.__class__))
+           http_paths = getattr(self.plugin,'_http',[])
+           for hid,http in http_paths.items():
+               if hid == 'root':
+                   target = os.path.join(fpath,http)
+                   for rtype in os.listdir(target):
+                       resource = queryUtility(IHTTPRoot,rtype)
+                       if not resource: continue
+                       resource.add_resources(os.path.join(target,rtype))
+
+
--- bit/bot/http/plugin.py
+++ bit/bot/http/plugin.py
@@ -6,17 +6,16 @@ from zope.component import getUtility, provideAdapter
 from twisted.web import server
 from twisted.application.internet import TCPServer, SSLServer
 
-from bit.core.interfaces import IConfiguration, IPlugin, ISockets
+from bit.core.interfaces import IConfiguration, IPlugin, ISockets, IPluginExtender
 from bit.bot.common.interfaces import IHTTPRoot, IWebRoot, IFlatten, IBotSocket, ISocketRequest
 
 from bit.bot.base.plugin import BotPlugin
-
 from bit.bot.http.root import HTTPRoot
 from bit.bot.http.socket import WebBotSocketFactory, Sockets
 from bit.bot.http.request import AuthRequest, SubscribeRequest, MessageRequest, HeloRequest, CommandRequest
 from bit.bot.http.handlers import socket_created, socket_lost
 from bit.bot.http.flat import SocketsFlattener
-
+from bit.bot.http.extends import HTTPPlugin
 
 class SSLContextFactory(object):
    def getContext(self):
@@ -41,6 +40,7 @@ class BotHTTP(BotPlugin):
         super(BotHTTP,self).load_services()        
 
     def load_adapters(self):
+        provideAdapter(HTTPPlugin,[IPlugin,],IPluginExtender)        
         provideAdapter(SocketsFlattener,[ISockets,],IFlatten)        
         provideAdapter(AuthRequest,[IBotSocket,],ISocketRequest,name='auth')        
         provideAdapter(MessageRequest,[IBotSocket,],ISocketRequest,name='message')        
